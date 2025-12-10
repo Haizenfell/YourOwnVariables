4910d2e2b3d26938d9803e6afbf321ad994559b4
Haizenfell <bilkevych.valerii@gmail.com>
2025-12-10 16:09:04 +0100

    4.6

--- src/main/java/yov/storage/BatchCapable.java
+++ src/main/java/yov/storage/BatchCapable.java
@@ -1,13 +1,3 @@
-/*
- * This file is part of YourOwnVariables.
- * Copyright (C) 2025 Haizenfell
- *
- * Licensed under the YourOwnVariables Proprietary License.
- * Unauthorized copying, modification, distribution, or reverse engineering
- * of this software is strictly prohibited.
- *
- * Full license text is provided in the LICENSE file.
- */
 package yov.storage;
 
 public interface BatchCapable {
